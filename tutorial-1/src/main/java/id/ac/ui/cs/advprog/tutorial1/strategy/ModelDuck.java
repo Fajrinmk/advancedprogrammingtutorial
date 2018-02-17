31b670169e008b00e98d60b74ad37bed23c8460b
fajrinmk <fajrinmk@gmail.com>
2018-02-17 20:59:28 +0700

    Complete all classes that represent variant of Duck, i.e. MallardDuckand ModelDuck

--- tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/ModelDuck.java
+++ tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/ModelDuck.java
@@ -1,5 +1,16 @@
 package id.ac.ui.cs.advprog.tutorial1.strategy;
 
 public class ModelDuck {
-    // TODO Complete me!
+	// TODO Complete me!
+	public void ModelDuck() {
+		this.setFlyBehavior(new FlyNoWay());
+		this.setQuackBehavior(new Squeak());
+		this.setSwimBehavior(new Swim());
+	}
+
+	@Override
+	public void display() {
+		System.out.println("I'm model duck");
+	}
+
 }
