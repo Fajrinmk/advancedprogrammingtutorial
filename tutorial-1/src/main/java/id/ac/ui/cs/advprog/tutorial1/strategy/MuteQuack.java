742790f3c2394278ea0086e67478a2ea5d529637
fajrinmk <fajrinmk@gmail.com>
2018-02-17 21:07:09 +0700

    Complete all classes that use QuackBehavior to implement quacking behaviorin duck, i.e. Quack, Squeak, and MuteQuack

--- tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/MuteQuack.java
+++ tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/MuteQuack.java
@@ -2,4 +2,9 @@ package id.ac.ui.cs.advprog.tutorial1.strategy;
 
 public class MuteQuack {
     // TODO Complete me!
+	 @Override
+	    public void quack() {
+	        System.out.println("<< Silence >>");
+	    }
+
 }
--- tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/Squeak.java
+++ tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/Squeak.java
@@ -2,4 +2,9 @@ package id.ac.ui.cs.advprog.tutorial1.strategy;
 
 public class Squeak {
     // TODO Complete me!
+	@Override
+    public void quack() {
+        System.out.println("Squeak");
+    }
+
 }
