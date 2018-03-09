7da8d4bbc1a3a4a938e5d94812d4fcede68a3932
fajrinmk <fajrinmk@gmail.com>
2018-03-09 18:41:44 +0700

    lengkapin TO DO PizzaTestDrive.java

--- tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise1/PizzaTestDrive.java
+++ tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise1/PizzaTestDrive.java
@@ -4,19 +4,31 @@ import id.ac.ui.cs.advprog.tutorial4.exercise1.pizza.Pizza;
 
 public class PizzaTestDrive {
 
-    public static void main(String[] args) {
-        PizzaStore nyStore = new NewYorkPizzaStore();
+	public static void main(String[] args) {
+		PizzaStore nyStore = new NewYorkPizzaStore();
 
-        Pizza pizza = nyStore.orderPizza("cheese");
-        System.out.println("Ethan ordered a " + pizza + "\n");
+		Pizza pizza = nyStore.orderPizza("cheese");
+		System.out.println("Ethan ordered a " + pizza + "\n");
 
-        pizza = nyStore.orderPizza("clam");
-        System.out.println("Ethan ordered a " + pizza + "\n");
+		pizza = nyStore.orderPizza("clam");
+		System.out.println("Ethan ordered a " + pizza + "\n");
 
-        pizza = nyStore.orderPizza("veggie");
-        System.out.println("Ethan ordered a " + pizza + "\n");
+		pizza = nyStore.orderPizza("veggie");
+		System.out.println("Ethan ordered a " + pizza + "\n");
 
-        // TODO Complete me!
-        // Create a new Pizza Store franchise at Depok
-    }
+		// TODO Complete me!
+		// Create a new Pizza Store franchise at Depok
+
+		PizzaStore depokStore = new DepokPizzaStore();
+
+		Pizza depokPizza = depokStore.orderPizza("cheese");
+		System.out.println("Nabil ordered a " + depokPizza + "\n");
+
+		depokPizza = depokStore.orderPizza("clam");
+		System.out.println("Nabil ordered a " + depokPizza + "\n");
+
+		depokPizza = depokStore.orderPizza("veggie");
+		System.out.println("Nabil ordered a " + depokPizza + "\n");
+	}
+
 }
