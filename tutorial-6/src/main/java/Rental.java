e990c9f7e272bb2311079d13b9cdcb8fbe2f446b
fajrinmk <fajrinmk@gmail.com>
2018-03-23 22:13:17 +0700

    Move the extracted method from step (1) to Rental class usingMove Method refactoring

--- tutorial-6/src/main/java/Rental.java
+++ tutorial-6/src/main/java/Rental.java
@@ -15,4 +15,23 @@ class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+
+	double lineAmount(double thisAmount) {
+		switch (getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		        thisAmount += 2;
+		        if (getDaysRented() > 2)
+		            thisAmount += (getDaysRented() - 2) * 1.5;
+		        break;
+		    case Movie.NEW_RELEASE:
+		        thisAmount += getDaysRented() * 3;
+		        break;
+		    case Movie.CHILDREN:
+		        thisAmount += 1.5;
+		        if (getDaysRented() > 3)
+		            thisAmount += (getDaysRented() - 3) * 1.5;
+		        break;
+		}
+		return thisAmount;
+	}
 }
