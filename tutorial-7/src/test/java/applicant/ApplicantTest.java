03d55718af6219205f8d1b5abfdf7377423cd5a2
fajrinmk <fajrinmk@gmail.com>
2018-04-06 22:01:01 +0700

    add test for applicant

--- tutorial-7/src/test/java/applicant/ApplicantTest.java
+++ tutorial-7/src/test/java/applicant/ApplicantTest.java
@@ -1,7 +1,50 @@
 package applicant;
 
+import java.util.function.Predicate;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ApplicantTest {
-    // TODO Implement me!
-    // Increase code coverage in Applicant class
-    // by creating unit test(s)!
+    private Predicate<Applicant> creditEvaluator;
+    private Predicate<Applicant> criminalRecordsEvaluator;
+    private Predicate<Applicant> employmentEvaluator;
+    private Predicate<Applicant> qualifiedEvaluator;
+    private Applicant applicant;
+
+    @Before
+    public void setUp() {
+        applicant = new Applicant();
+        qualifiedEvaluator = Applicant::isCredible;
+        creditEvaluator = anApplicant -> anApplicant.getCreditScore() > 600;
+        employmentEvaluator = anApplicant -> anApplicant.getEmploymentYears() > 0;
+        criminalRecordsEvaluator = anApplicant -> !anApplicant.hasCriminalRecord();
+    }
+
+    @Test
+    public void testApplicantOneisQualified() {
+        Assert.assertEquals(true, Applicant.evaluate(applicant, qualifiedEvaluator));
+    }
+
+    @Test
+    public void testApplicantTwotisQualified() {
+        Assert.assertEquals(true, Applicant.evaluate(applicant, qualifiedEvaluator
+                .and(employmentEvaluator)));
+    }
+
+    @Test
+    public void testApplicantThreeisQualified() {
+        Assert.assertEquals(false, Applicant.evaluate(applicant, qualifiedEvaluator
+                .and(employmentEvaluator)
+                .and(criminalRecordsEvaluator)));
+    }
+
+    @Test
+    public void testApplicantFourisQualified() {
+        Assert.assertEquals(false, Applicant.evaluate(applicant, qualifiedEvaluator
+                .and(employmentEvaluator)
+                .and(creditEvaluator)
+                .and(criminalRecordsEvaluator)));
+    }
 }
