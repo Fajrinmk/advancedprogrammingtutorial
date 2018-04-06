4e80b076a44488410e7b0096b2d9e39137f48346
fajrinmk <fajrinmk@gmail.com>
2018-04-06 16:58:50 +0700

    finish 2nd exercise + test

--- tutorial-7/src/main/java/WordCount.java
+++ tutorial-7/src/main/java/WordCount.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -10,25 +9,12 @@ import java.nio.file.Paths;
 public class WordCount {
 
     public static long countLines(String word, Path file) throws IOException {
-        long count = 0;
-
-        BufferedReader reader = new BufferedReader(new FileReader(file.toString()));
-        String line = null;
-
-        while ((line = reader.readLine()) != null) {
-            if (line.contains(word)) {
-                count++;
-            }
-        }
-
-        reader.close();
-
-        return count;
+        return Files.lines(file).filter(line -> line.contains(word)).count();
     }
 
     public static void main(String[] args) {
         String word = "for";
-        String filePath = "tutorial-7/aTextFile.txt";
+        String filePath = "aTextFile.txt";
         Path file = Paths.get("", filePath);
 
         try {
